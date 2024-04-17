1cdc197e3e09e75bca4d61258ff7737a32511d4a
tonniegeetz <tonniegeetz@gmail.com>
2024-04-17 05:32:41 +0300

    Understand DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function () {
+  const text = document.getElementById('text');
+  text.textContent = 'This is really cool!';
+});
+
+//there is also a load event that is used when everything including the CSS and the images are completely loaded
